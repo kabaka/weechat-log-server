418207634f010e7e653ac9b817bc91c9d8b26334
Kyle Jonhson <kyle@vacantminded.com>
2013-04-15 05:15:54 +0000

    require HTTP auth for indexes, keys for log view
    * Added --user-name/-u and --password/-p option for auth. Default
    admin/admin.
    * -p changed to -P (port arg)
    A per-run key is generated for each log view on load, and can be used in
    the URL to grant un-authenticated access to log pages.

--- lib/weechat-log-server.rb
+++ lib/weechat-log-server.rb
@@ -2,8 +2,11 @@ require 'thin'
 require 'sinatra'
 require 'socket'
 require 'htmlentities'
+require 'securerandom'
 
 require 'weechat-log-server/webserver'
 require 'weechat-log-server/string'
+
+$keys = {}
 
 Thin::Server.start Webserver, $opts[:bind_address], $opts[:port]
--- lib/weechat-log-server/webserver.rb
+++ lib/weechat-log-server/webserver.rb
@@ -3,19 +3,57 @@ class Webserver < Sinatra::Base
     set :views, File.expand_path($opts[:templates_dir])
   end
 
+  get '/test/?' do
+    request.inspect
+  end
+
   get '/' do
     erb :index
   end
 
   get '/:network/?' do
+    auth_required!
     erb :network
   end
 
   get '/:network/:buffer/?' do
+    auth_required!
     erb :buffer
   end
 
   get '/:network/:buffer/:date/?' do
+    unless key! "/#{params[:network]}/#{params[:buffer]}/#{params[:date]}", params[:key]
+      auth_required!
+    end
+
     erb :buffer_date
+  end
+
+  helpers do
+
+    def auth_required!
+      unless authorized?
+        response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+        throw :halt, [401, "Not authorized\n"]
+      end
+    end
+
+    def authorized?
+      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [$opts[:user_name], $opts[:password]]
+    end
+
+    
+    def set_key route
+      $keys[route] ||= generate_key
+    end
+
+    def generate_key
+      SecureRandom.urlsafe_base64
+    end
+
+    def key! route, key
+      $keys.has_key? route and $keys[route] == key
+    end
   end
 end
